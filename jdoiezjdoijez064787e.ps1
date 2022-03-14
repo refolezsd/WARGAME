675037ba64cbe0c131f6cdae868f9ee6a0afd4e1
refolezsd <101591143+refolezsd@users.noreply.github.com>
2022-03-14 18:46:20 +0100

    Add files via upload

---
+++ jdoiezjdoijez064787e.ps1
@@ -0,0 +1,3 @@
+﻿$wshell = New-Object -ComObject Wscript.Shell
+$decodemessage=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String('RQBTAEQAewBTAHkAdQByAGcAdQB0AGEAfQA='))
+$wshell.Popup($decodemessage,0,"Done",0x1)
